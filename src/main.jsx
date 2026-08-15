c65a49bed92e55f8c11286113b8ad97611e06a0b
kodlbegiko <sean112124@gmail.com>
2026-08-15 18:55:23 +0800

    feat: mount research achievement system

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
+import AchievementSystem from './components/AchievementSystem.jsx';
 import './styles.css';
+import './achievements.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
+    <AchievementSystem />
   </React.StrictMode>,
 );
