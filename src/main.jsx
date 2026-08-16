fcf7b06122799e611943e386da17401ebcb5d00a
kodlbegiko <sean112124@gmail.com>
2026-08-16 09:57:40 +0800

    Use polished roadmap and final UI refinement layer

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './data/recoveryOverlay.js';
 import App from './App.jsx';
-import InteractiveAchievementSystem from './components/InteractiveAchievementSystem.jsx';
+import PolishedAchievementSystem from './components/PolishedAchievementSystem.jsx';
 import { LanguageProvider } from './i18n.jsx';
 import './styles.css';
 import './achievements.css';
@@ -10,12 +10,13 @@ import './research-lines.css';
 import './roadmap-interactions.css';
 import './macbook.css';
 import './i18n.css';
+import './polish.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <LanguageProvider>
       <App />
-      <InteractiveAchievementSystem />
+      <PolishedAchievementSystem />
     </LanguageProvider>
   </React.StrictMode>,
 );
